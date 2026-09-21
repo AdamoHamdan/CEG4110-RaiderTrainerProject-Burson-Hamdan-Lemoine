188040cfece4012aa9a66fda9c025238eb5b2a95
Will42424 <burson.7@wright.edu>
2026-09-21 12:24:27 -0400

    Update samplePoseTracker.py

--- ApplicationWork/samplePoseTracker.py
+++ ApplicationWork/samplePoseTracker.py
@@ -1,11 +1,15 @@
 import cv2
 from ultralytics import YOLO
+import numpy as np
 
 #Load pre-trained model. n indicates that it is the nano size, i.e. the smallest model. The example uses YOLOv8, you can also use yolo11n-pose.pt or yolo26n-pose.pt.
 model = YOLO("yolov8n-pose.pt")
 
 #Initialize the camera. This example uses a webcam which is by default 0. 
 inputCamera = cv2.VideoCapture(0)
+
+rested = False
+succesful = 0
 
 #Loop as long as inputCamera remains open. In other words, you can quit by pressing 'q' or if the camera crashes. 
 while inputCamera.isOpened():
@@ -31,13 +35,25 @@ while inputCamera.isOpened():
         RElbow = body[8]
         LWrist = body[9]
         RWrist = body[10]
-        print("LShoulder:",LShoulder)
-        print("RShoulder:",RShoulder)
-        print("LElbow:",LElbow)
-        print("RElbow:",RElbow)
-        print("LWrist:",LWrist)
-        print("RWist:",RWrist) 
-
+       # print("LShoulder:",LShoulder)
+       # print("RShoulder:",RShoulder)
+       # print("LElbow:",LElbow)
+       # print("RElbow:",RElbow)
+       # print("LWrist:",LWrist)
+       # print("RWist:",RWrist) 
+        ElbowShoulder = np.array(RShoulder) - np.array(RElbow)
+        ElbowWrist = np.array(RWrist) - np.array(RElbow)
+        cosine_angle = np.dot(ElbowShoulder, ElbowWrist) / (np.linalg.norm(ElbowShoulder) * np.linalg.norm(ElbowWrist))
+        angle = np.arccos(cosine_angle)
+        print (np.degrees(angle))
+        if np.degrees(angle) > 150:
+          rested = True
+        print (rested)
+        if np.degrees(angle) < 40 and rested:
+          rested = False
+          succesful = succesful + 1
+        print (succesful)
+          
 
    
     
